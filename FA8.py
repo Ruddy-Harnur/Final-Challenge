e3534431871ea1fdc0b7bbfe87e6d7fabdaa4f8e
Ruddy-Harnur <58189651+Ruddy-Harnur@users.noreply.github.com>
2020-05-17 12:41:21 -0700

    Add files via upload

--- FA8.py
+++ FA8.py
@@ -92,7 +92,6 @@ if __name__=='__main__':
                           (((pv['HN_int']%2!=0) & (pv['HN_int'] >= centerline['L_LOW_int']) & (pv['HN_int'] <= centerline['L_HIGH_int'])) |
                           ((pv['HN_int']%2==0) & (pv['HN_int'] >= centerline['R_LOW_int']) & (pv['HN_int'] <= centerline['R_HIGH_int']))))
                           
-    result_df = result_df.join(broadcast(centerline), ['PHYSICALID'])
     result_df = result_df.select('PHYSICALID', '2015', '2016', '2017', '2018', '2019')
     result_df = result_df.orderBy('PHYSICALID')
     result_df =result_df.groupBy('PHYSICALID').agg({'2015' : 'sum', '2016':'sum', '2017':'sum', '2018':'sum', '2019':'sum'})
@@ -114,3 +113,8 @@ if __name__=='__main__':
                                                
     result_df = result_df.orderBy('PHYSICALID')
     result_df.write.csv(sys.argv[1])
+    
+    starttime = datetime.now()
+    run_spark(str(p.output_path))
+    elapsed = datetime.now() - starttime
+    print("Done, Elapsed: {} (secs)".format(elapsed.total_seconds()))
